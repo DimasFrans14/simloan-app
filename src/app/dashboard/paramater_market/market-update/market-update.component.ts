1720e5d2b92a95dab55fab35e2f6344f1a8fc7a8
DimasFrans14 <dimasfranss@gmail.com>
2023-11-13 15:53:27 +0700

    component parameter market

---
+++ src/app/dashboard/paramater_market/market-update/market-update.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-market-update',
+  templateUrl: './market-update.component.html',
+  styleUrls: ['./market-update.component.css']
+})
+export class MarketUpdateComponent {
+
+}
