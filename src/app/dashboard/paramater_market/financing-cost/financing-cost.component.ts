595eb823a7c3e42b048adbd911443445766f509e
DimasFrans14 <dimasfranss@gmail.com>
2023-11-13 15:53:27 +0700

    component parameter market

---
+++ src/app/dashboard/paramater_market/financing-cost/financing-cost.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-financing-cost',
+  templateUrl: './financing-cost.component.html',
+  styleUrls: ['./financing-cost.component.css']
+})
+export class FinancingCostComponent {
+
+}
