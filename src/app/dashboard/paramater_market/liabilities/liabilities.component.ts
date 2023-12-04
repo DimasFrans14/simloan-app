5e085d3dfaa593318c9ed3c7b2e137ee9df3705b
DimasFrans14 <dimasfranss@gmail.com>
2023-12-04 10:11:50 +0700

    read excel data to json in liabilities

--- src/app/dashboard/paramater_market/liabilities/liabilities.component.ts
+++ src/app/dashboard/paramater_market/liabilities/liabilities.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-liabilities',
@@ -8,10 +9,42 @@ import { Component } from '@angular/core';
 export class LiabilitiesComponent {
 
   files: File[] = [];
+  excelDataJSON: any;
+  excelDataTable: any;
+
+  readExcel(event: any){
+    let file = event.addedFiles[0];
+    let fileReader = new FileReader();
+    fileReader.readAsBinaryString(file);
+
+    fileReader.onload = (e) => {
+
+      var workbook = XLSX.read(fileReader.result, {type:'binary'});
+      var sheetNames = workbook.SheetNames;
+      this.excelDataJSON =  XLSX.utils.sheet_to_json(workbook.Sheets[sheetNames[0]]);
+      this.excelDataTable = XLSX.utils.sheet_to_html(workbook.Sheets[sheetNames[0]]);
+
+      // this.html = this.sanitizer.bypassSecurityTrustHtml(this.excelDataTable);
+      console.log(fileReader);
+      console.log(this.excelDataJSON);
+
+    //   this.tableUpdate = new Tabulator(".example-table-2", {
+    //     height:205,
+    //     data:this.excelDataJSON,
+    //     layout:"fitDataTable",
+    //     columns:[
+    //       {title:"Name", field:"Name", width:150, editor:"input"},
+    //       {title:"Birthday", field:"Birthday", hozAlign:"left"},
+    //     ],
+    //  });
+    }
+  }
+
 
   onSelect(event: { addedFiles: any; }) {
     console.log(event);
     this.files.push(...event.addedFiles);
+    this.readExcel(event)
   }
 
   onRemove(event: File) {
