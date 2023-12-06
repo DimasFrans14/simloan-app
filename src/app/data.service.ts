ccfd794d1e246b3acce2d5ff69d8ff215002d436
DimasFrans14 <dimasfranss@gmail.com>
2023-12-06 20:22:36 +0700

    data services

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,21 +2,40 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 
+import { TableServicesService } from './services/table_services/table-services.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private dataT: TableServicesService) { }
 
-  async getData(){
+  result: any;
+
+  async fetchDataKurs(){
     try {
       return await lastValueFrom(
-        this.http.get('https://jsonplaceholder.typicode.com/users')
+        this.http.get('http://10.1.18.47:9051/simloan/ws-v01/master-kurs/list')
       );
     } catch (error) {
       console.log(error);
       return null;
+    }
+  }
+
+  async setData() {
+    try {
+      const data = await this.fetchDataKurs();
+      if (data) {
+        // Assuming 'result' is the variable where you want to store the data
+        this.result = data;
+        console.log(this.result); // Check the retrieved data
+      } else {
+        console.log('No data retrieved');
+      }
+    } catch (error) {
+      console.error(error);
     }
   }
 
