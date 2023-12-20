4fe652715af1bfacd1d427a6a00ace7070b1f114
RifqiMR17 <62689822+RifqiMR17@users.noreply.github.com>
2023-12-20 14:55:49 +0700

    updateProgressFindebt

--- src/app/dashboard/shl/shl-agreement/create-agreement/create-agreement.component.ts
+++ src/app/dashboard/shl/shl-agreement/create-agreement/create-agreement.component.ts
@@ -1,4 +1,6 @@
+import { CloseScrollStrategy } from '@angular/cdk/overlay';
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-create-agreement',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-agreement.component.css']
 })
 export class CreateAgreementComponent {
+
+  convertedJson!:string;
+
+  fileUpload(event:any){
+    console.log(event.target.files);
+    const selectedFile = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.readAsBinaryString(selectedFile);
+    fileReader.onload =  (event:any) => {
+      console.log(event);
+      let binaryData = event.target.result;
+      let workbook = XLSX.read(binaryData, {type:'binary'})
+      console.log(workbook);
+      workbook.SheetNames.forEach(sheet =>{
+        const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+        console.log(data);
+        this.convertedJson = JSON.stringify(data, undefined, 6);
+      })
+      console.log(workbook)
+    }
+  }
 
 }
