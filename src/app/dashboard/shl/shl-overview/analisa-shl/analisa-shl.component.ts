bf0d630c01cc909b307ae4b73101ad2ac7c026b0
RifqiMR17 <62689822+RifqiMR17@users.noreply.github.com>
2024-01-04 11:01:22 +0700

    update_frtchAPI_analisaSHL

--- src/app/dashboard/shl/shl-overview/analisa-shl/analisa-shl.component.ts
+++ src/app/dashboard/shl/shl-overview/analisa-shl/analisa-shl.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { DataService } from 'src/app/data.service';
 
 @Component({
   selector: 'app-analisa-shl',
   templateUrl: './analisa-shl.component.html',
   styleUrls: ['./analisa-shl.component.css']
 })
-export class AnalisaShlComponent {
+export class AnalisaShlComponent{
 
+  constructor(private dataService: DataService){
+
+  }
+
+  data: any;
+
+  ngOnInit() {
+    this.dataService.getDataAnalisaSHL().subscribe(data => {
+      this.data = data;
+    });
+  }
 }
