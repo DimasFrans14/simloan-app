025c43ed0181641a09329ef776756081a7b2595f
DimasFrans14 <dimasfranss@gmail.com>
2024-04-17 15:22:14 +0700

    fix env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
-    apiUrl1: 'http://10.1.18.47:9051',
-    apiUrl2: 'http://10.1.18.47:8080/simloan-ws', //dev server
+  production: true,
+  apiUrl1: 'http://10.1.85.44:9051',
+  apiUrl2: 'http://10.1.85.44:8180/simloan-prod-ws',
 };
